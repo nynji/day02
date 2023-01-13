824ea34b55d30906d06bdd7122c49495ff4d2a61
HJ <12210544a@inha.edu>
2023-01-13 15:11:40 +0900

    seokchae

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+
+PI = 3.14
+print(f'원주율의 값은 {PI}이고 타입은 {type(PI)}입니다.')
+
+#chap 3
+
+#진법
+
+#number = 0b10011010
+#number = 0x9A
+#number = 0o232
+#print(number)
+
+number = 154
+print(bin(number))
+print(hex(number))
+print(oct(number))
+
+print(ord("a")) #아스키
+print(hex(ord(" ")))
+
 PI = 3.14
 print(f'원주율의 값은 {PI}이고 타입은 {type(PI)}입니다.')
